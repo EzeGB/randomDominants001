d5bb6e2c3713eef7b46b3d08a2d4f84b89e95c2e
Zeke <ezequielgerstel777@gmail.com>
2024-05-18 16:05:24 -0400

    programa acabado

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,18 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String [] tipo = {"sus","#11","#9","b9#11","#9b13","b913"};
-        String [] notas = {"C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"};
-        int randTipo = (int)(Math.random()*5);
-        int randNotas = (int)(Math.random()*11);
-        System.out.println(notas[randNotas]+tipo[randTipo]);
+        String [] tipo = {"sus","#11","#9","(b9#11)","#9b13","(b913)"};
+        String [] notas = {"C7","Db7","D7","Eb7","E7","F7","Gb7","G7","Ab7","A7","Bb7","B7"};
+
+        for (int i=0; i<32;i++){
+            int randTipo = (int)(Math.random()*5);
+            int randNotas = (int)(Math.random()*11);
+            System.out.print(notas[randNotas]+tipo[randTipo]);
+            if ((i+1)%4==0){
+                System.out.println();
+            } else {
+                System.out.print("-");
+            }
+        }
     }
 }
