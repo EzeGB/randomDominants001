f790e8981737d9a43521ec70dc9a7ad5c3037e3b
Zeke <ezequielgerstel777@gmail.com>
2024-05-18 15:52:04 -0400

    generacion aleatoria acorde dominante

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         String [] tipo = {"sus","#11","#9","b9#11","#9b13","b913"};
         String [] notas = {"C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"};
-        System.out.println(notas[4]+tipo[2]);
+        int randTipo = (int)(Math.random()*5);
+        int randNotas = (int)(Math.random()*11);
+        System.out.println(notas[randNotas]+tipo[randTipo]);
     }
 }
