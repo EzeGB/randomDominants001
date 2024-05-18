22ec457839d2bb4297a966928cde13889731e373
Zeke <ezequielgerstel777@gmail.com>
2024-05-18 15:45:55 -0400

    cargado elementos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String [] tipo = {"sus","#11","#9","b9#11","#9b13","b913"};
+        String [] notas = {"C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"};
+        System.out.println(notas[4]+tipo[2]);
     }
 }
